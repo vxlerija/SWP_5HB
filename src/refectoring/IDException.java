9879f266a1d4c21a5c4a14ab053456da75dcc352
vxlerija <valerija.klotz@hak-reutte.ac.at>
2026-03-24 09:01:17 +0100

    wurde gelöscht, da die Klasse nie verwendet wird

--- src/refectoring/IDException.java
+++
@@ -1,9 +0,0 @@
-package refectoring;
-
-public class IDException extends Exception{
-	private static final long serialVersionUID = 1L;
-
-	public IDException(String message) {
-		super(message);
-	}
-}
